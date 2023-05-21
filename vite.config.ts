0d3898918380733811c74408a2ba3e6f5f6d037b
Nam077 <nam077.me@gmail.com>
2023-05-21 21:10:21 +0700

    update v2

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: './vku-score-v2',
+    base: '/vku-score-v2',
 });
